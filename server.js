908a6dcd6cfe517e88533b4eebdc3e30690facbd
KarLOL82 <klinfeldt@gmail.com>
2022-08-23 19:41:46 -0700

    need to write json file as an array

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const notes = require("./db/db.json");
 const fs = require("fs");
 const path = require("path");
+const uuid = require("./public/assets/uuid");
 
 const PORT = 3001;
 
@@ -10,54 +11,49 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("/api/notes", (req, res) => res.status(200).json(notes));
 
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
 
-app.get('/api/notes', (req, res) =>
-res.status(200).json(notes));
+  const { title, text } = req.body;
 
-app.post('/api/notes', (req, res) => {
-    
-    console.info(`${req.method} request received to add a note`);
-  
-    
-    const { title, text } = req.body;
-  
-    
-    if (title && text) {
-     
-      const newNote = {
-        text,
-        title,
-        // note_id: uuid(),
-      };
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting note');
-    }
-  });
+  if (title && text) {
+    const newNote = {
+      text,
+      title,
+      note_id: uuid(),
+    };
 
-  app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
+    const noteString = JSON.stringify(newNote);
+
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(`A new note for ${newNote} has been written to JSON file`)
+    );
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-
-//   fs.writeFile
-
-
-
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
