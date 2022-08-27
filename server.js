cefa1b5a3fa69d55d4a88429879777e53a4a1df7
KarLOL82 <klinfeldt@gmail.com>
2022-08-26 23:47:48 -0700

    attempting delete route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Required files and modules
 const express = require("express");
 const notes = require("./db/db.json");
 const fs = require("fs");
@@ -8,6 +9,7 @@ const PORT = 3001;
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,18 +19,21 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// GET route to read saved notes from db.json file
 app.get("/api/notes", (req, res) => res.json(notes));
 
+// Post route starts here
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a new note`);
 
+  // Deconstructs the object, builds new note and pushes it to the array as string data
   const { text, title } = req.body;
 
   if (text && title) {
     const newNote = {
       text,
       title,
-      note_id: uuid(),
+      id: uuid(),
     };
     notes.push(newNote);
 
@@ -49,6 +54,33 @@ app.post("/api/notes", (req, res) => {
   }
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+           parsedNotes = JSON.parse(data);
+          const notesDecremented = parsedNotes.filter(notes => notes.id !== parseInt(req.params.id));
+      fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(notesDecremented, null, 2),
+          (writeErr) =>
+           writeErr
+            ? console.error(writeErr)
+            : console.info("Successfully updated notes!")
+          );
+          const response = {
+            status: "success",
+            body: notesDecremented,
+          };
+          console.log(response);
+          res.json(notes);
+        } 
+    });
+      
+});
+
+
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
@@ -58,19 +90,3 @@ app.listen(PORT, () =>
 );
 
 
-// fs.readFile("./db/db.json", "utf8", (err, data) => {
-    //     if (err) {
-    //       console.error(err);
-    //     } else {
-    //        parsedNotes = JSON.parse(data);
-    //       parsedNotes.push(newNote);
-    //   fs.writeFile(
-    //       "./db/db.json",
-    //       JSON.stringify(parsedNotes, null, 2),
-    //       (writeErr) =>
-    //        writeErr
-    //         ? console.error(writeErr)
-    //         : console.info("Successfully updated notes!")
-    //       );
-    //     }
-    //    })
